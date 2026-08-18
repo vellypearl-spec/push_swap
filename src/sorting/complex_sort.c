055f2eb5044edb4a9672f8a05fbb6f78a14f0548
giftlouis04-cmd <giftlouis04@gmail.com>
2026-08-18 10:38:02 +0200

    style: make complex_sort Norminette-friendly (split helper, tabs, spacing)

--- src/sorting/complex_sort.c
+++ src/sorting/complex_sort.c
@@ -12,52 +12,62 @@
 
 #include "push_swap.h"
 
-static int get_max_bits(int n)
+static int	get_max_bits(int n)
 {
-int bits;
-int max;
+	int	bits;
+	int	max;
 
-bits = 0;
-max = n - 1;
-while (max > 0)
-{
-    max >>= 1;
-    bits++;
-}
-return (bits);
+	bits = 0;
+	max = n - 1;
+	while (max > 0)
+	{
+		max >>= 1;
+		bits++;
+	}
+	return (bits);
 }
 
-void complex_sort(t_node **a, t_node **b)
+static int	handle_small_stack(t_node **a)
 {
-int bits;
-int bit;
-int i;
-int len;
+	int	size;
 
-if (stack_size(*a) <= 3)
-{
-    if (stack_size(*a) == 2)
-        sort_two(a);
-    else if (stack_size(*a) == 3)
-        sort_three(a);
-    return ;
+	size = stack_size(*a);
+	if (size <= 3)
+	{
+		if (size == 2)
+			sort_two(a);
+		else if (size == 3)
+			sort_three(a);
+		return (1);
+	}
+	return (0);
 }
-bits = get_max_bits(stack_size(*a));
-bit = 0;
-while (bit < bits)
+
+void	complex_sort(t_node **a, t_node **b)
 {
-    len = stack_size(*a);
-    i = 0;
-    while (i < len)
-    {
-        if (((*a)->index >> bit) & 1)
-            pb(a, b);
-        else
-            ra(a);
-        i++;
-    }
-    while (*b)
-        pa(a, b);
-    bit++;
-    }
+	int	bits;
+	int	bit;
+	int	i;
+	int	len;
+
+	if (handle_small_stack(a))
+		return;
+	bits = get_max_bits(stack_size(*a));
+	bit = 0;
+	while (bit < bits)
+	{
+		len = stack_size(*a);
+		i = 0;
+		while (i < len)
+		{
+			if (((*a)->index >> bit) & 1)
+				pb(a, b);
+			else
+				ra(a);
+			i++;
+		}
+		while (*b)
+			pa(a, b);
+		bit++;
+	}
 }
