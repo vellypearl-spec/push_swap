eb4e4927ef3fbf52bbe83c6ff33f769b915af620
giftlouis04-cmd <giftlouis04@gmail.com>
2026-08-18 10:05:44 +0200

    Add complex_sort function for linked list sorting
    Implement complex sorting algorithm for linked list.

---
+++ src/sorting/complex_sort.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   complex_sort.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glouis <glouis@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/18 10:02:30 by glouis            #+#    #+#             */
+/*   Updated: 2026/08/18 10:02:30 by glouis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int get_max_bits(int n)
+{
+int bits;
+int max;
+
+bits = 0;
+max = n - 1;
+while (max > 0)
+{
+    max >>= 1;
+    bits++;
+}
+return (bits);
+}
+
+void complex_sort(t_node **a, t_node **b)
+{
+int bits;
+int bit;
+int i;
+int len;
+
+if (stack_size(*a) <= 3)
+{
+    if (stack_size(*a) == 2)
+        sort_two(a);
+    else if (stack_size(*a) == 3)
+        sort_three(a);
+    return ;
+}
+bits = get_max_bits(stack_size(*a));
+bit = 0;
+while (bit < bits)
+{
+    len = stack_size(*a);
+    i = 0;
+    while (i < len)
+    {
+        if (((*a)->index >> bit) & 1)
+            pb(a, b);
+        else
+            ra(a);
+        i++;
+    }
+    while (*b)
+        pa(a, b);
+    bit++;
+}
+}
